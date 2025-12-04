e55da739822d529bca05ee634e832f9e9e090c50
Alouakhalid <143850860+Alouakhalid@users.noreply.github.com>
2025-12-04 21:47:00 +0200

    Create E - Area of a Circle.cpp

---
+++ sheet1/E - Area of a Circle.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout << fixed << setprecision(9);
+    double r;
+    double pi = 3.14159265358979323846;
+    cin>>r;
+    cout<<pi*pow(r,2)<<endl;
+}
