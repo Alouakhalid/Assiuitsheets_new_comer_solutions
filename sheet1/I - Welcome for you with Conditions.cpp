8ac1b12ed969b63f6efbc6d887b66c0a1f9e1556
Alouakhalid <143850860+Alouakhalid@users.noreply.github.com>
2025-12-04 22:37:17 +0200

    Create I - Welcome for you with Conditions.cpp

---
+++ sheet1/I - Welcome for you with Conditions.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    long long a,b;
+    cin>>a>>b;
+    if (a<b)
+    {
+        cout<<"No"<<endl;
+    }
+    else
+    {
+        cout<<"Yes"<<endl;
+    }
+}
