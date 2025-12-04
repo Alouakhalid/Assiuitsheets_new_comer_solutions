579e7559beccd74cfb42672a0c7fc249532f12d8
Alouakhalid <143850860+Alouakhalid@users.noreply.github.com>
2025-12-04 23:13:21 +0200

    Create The Brothers.cpp

---
+++ sheet1/The Brothers.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string Firstname_1,Lastname_1;
+    string Firstname_2,Lastname_2;
+    cin>>Firstname_1>>Lastname_1>>Firstname_2>>Lastname_2;
+    if (Lastname_1 == Lastname_2)
+    {
+        cout<<"ARE Brothers"<<endl;
+
+    }
+    else
+    {
+        cout<<"NOT"<<endl;
+    }
+
+    return 0;
+}
