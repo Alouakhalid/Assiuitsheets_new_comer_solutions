756478a8a9a3b57837dd580f5eefae1de5521455
Alouakhalid <143850860+Alouakhalid@users.noreply.github.com>
2025-12-04 21:24:28 +0200

    Add files via upload

---
+++ sheet1/main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+  int n;
+  cin>>n;
+  long long c;
+  cin>>c;
+  char a;
+  cin>>a;
+  float r;
+  cin>>r;
+  double d;
+  cin>>d;
+  cout<<n<<endl;
+  cout<<c<<endl;
+  cout<<a<<endl;
+  cout<<r<<endl;
+  cout<<d<<endl;
+}
